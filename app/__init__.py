92f46a65383e41a09ca7ff0ee919fc85cfc49c54
Justin Upson <jupson@rocketmail.com>
2025-05-06 23:43:35 +0100

    Templates added

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from config import Config 
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config.from_object(Config)
 
+   
     db.init_app(app)
     login_manager.init_app(app)
 
@@ -18,4 +19,9 @@ def create_app():
 
     return app
 
-from . import models
+
+from .models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
--- run.py
+++ run.py
@@ -1,20 +1,6 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from app import create_app
 
-# Initialize the app
-app = Flask(__name__)
-app.config.from_object(Config)
+app = create_app()
 
-# Initialize the database
-db = SQLAlchemy(app)
-
-# Define the routes (Blueprint should still be used)
-from app.routes import main  # Importing the blueprint
-
-# Register the blueprint with the app
-app.register_blueprint(main)
-
-# Run the app
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
